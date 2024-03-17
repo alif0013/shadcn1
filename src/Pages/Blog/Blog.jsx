6d92dacbd9c62ba942f38aa9486944b771d999a8
alif0013 <mdalifh36@gmail.com>
2024-03-18 00:49:15 +0600

    added a login carousel

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel"
 
 const Blog = () => {
+    
+
+
     return (
-        <div className='mt-[80px]'>
-            This is blog page
+        <div className='mt-[100px] md:w-[1200px] mx-auto'>
+            <Carousel>
+
+                <CarouselContent>
+                
+                    <CarouselItem><img className="h-[350px] w-full" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.523418798.1710633600&semt=sph" alt="" /></CarouselItem>
+                    <CarouselItem><img className="h-[350px] w-full" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.523418798.1710633600&semt=sph" alt="" /></CarouselItem>
+                    <CarouselItem><img className="h-[350px] w-full" src="https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg?size=626&ext=jpg&ga=GA1.1.523418798.1710633600&semt=sph" alt="" /></CarouselItem>
+                 
+                </CarouselContent>
+                <CarouselPrevious />
+                <CarouselNext />
+            </Carousel>
+
         </div>
     );
 };
